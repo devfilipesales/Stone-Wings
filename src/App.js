1d172a777e20994a8ef9f89a5471942dd2017f8a
Filipe Sales Araujo <48739233+devfilipesales@users.noreply.github.com>
2019-05-27 14:43:20 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Album from './Album';
+
+function App() {
+  return (
+    <div className="App">
+      <Album></Album>
+    </div>
+  );
+}
+
+export default App;
